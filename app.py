71fd1764f7eea8fc8260b7df560bcaaec0cceda5
taesiri <mtaesiri@gmail.com>
2023-08-04 15:57:08 +0000

    latex cleaning

--- app.py
+++ app.py
@@ -9,6 +9,23 @@ def convert_and_compile(input_file):
     pdf_file = "output.pdf"
     png_file = "output.png"
 
+    # Define the helper functions
+    def separate_text_equations(text):
+        text = re.sub(r"\\\(", "\n\\(", text)
+        text = re.sub(r"\\\)", "\\)\n", text)
+        return text
+
+    def remove_unnecessary_latex_commands(text):
+        lines = text.split("\n")
+        for i in range(len(lines)):
+            line = lines[i]
+            if "\\RL" in line:
+                line = re.sub(r"\\RL\{([^}]*)\}", r"\1", line)
+            if "\\text" in line:
+                line = re.sub(r"\\text\{([^}]*)\}", r"\1", line)
+            lines[i] = line
+        return "\n".join(lines).strip()
+
     # Convert .docx to .md
     subprocess.run(["pandoc", "-s", docx_file, "-t", "markdown", "-o", "intermediate.md"], check=True)
 
@@ -19,6 +36,10 @@ def convert_and_compile(input_file):
     with open("intermediate.tex", "r") as f:
         content = f.read()
     content = re.search(r"\\begin\{document\}([\s\S]*)\\end\{document\}", content).group(1)
+
+    # Separate text and equations, and remove unnecessary LaTeX commands
+    content = separate_text_equations(content)
+    content = remove_unnecessary_latex_commands(content)
 
     # Generate a new .tex file
     with open(latex_file, "w") as f:
@@ -49,7 +70,8 @@ def convert_and_compile(input_file):
 
 iface = gr.Interface(
     fn=convert_and_compile, 
-    inputs=gr.File(label="Upload .docx"), 
-    outputs=gr.Image(type="pil", label="Output PDF as PNG"),
+    inputs=gr.File(label="Upload .DocX"), 
+    outputs=gr.Image(type="pil", label="Latex Output", show_label=False),
 )
-iface.launch(debug=True)
+
+iface.launch(debug=True, share=True, server_name="tehran.emma-jewellery.ir", server_port=21401, ssl_certfile="/root/cert.crt",  ssl_keyfile="/root/private.key")
